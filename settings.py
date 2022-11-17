8801ca0b11c3eadda774b2a096278585656e0241
Andr3ws-E <115025689+Andr3ws-E@users.noreply.github.com>
2022-11-17 12:45:46 -0800

    update

---
+++ settings.py
@@ -0,0 +1,20 @@
+### game settings ###
+#Width of the screen
+WIDTH = 360
+#hight of the game screen
+HEIGHT = 480
+# frames per seocnd in the game
+FPS = 30
+
+
+# player settings
+PLAYER_GRAV = 0.0
+PLAYER_FRIC = 0.9
+SCORE = 0
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
