b808932aaf8bfb5bbf15a4786d3b91c18a3b6247
Andr3ws-E <115025689+Andr3ws-E@users.noreply.github.com>
2022-11-17 12:37:03 -0800

    update

---
+++ Main.py
@@ -0,0 +1,12 @@
+'''
+Game Goals: move left and right to grab all 30 points with the most health possible
+'''
+# sources: 
+#  Mr. Cozort, http://kidscancode.org/blog/, https://www.w3schools.com
+
+#imports libraries
+import pygame as pg
+from pygame.sprite import Sprite
+import random
+from random import randint
+import time
